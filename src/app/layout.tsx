6e49008f3f65c2d2d8b27d470c031971551a9d14
Lucas Pazzim <lpazzim@gmail.com>
2023-11-16 00:26:21 -0300

    setup page.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,10 @@
+import { Metadata } from "next";
 import StyledJsxRegistry from "../../lib/registry";
+
+export const metadata: Metadata = {
+  title: "Lucas Pazzim",
+  description: "Lucas Pazzim - Frontend Engineer",
+};
 
 export default function RootLayout({
   children,
